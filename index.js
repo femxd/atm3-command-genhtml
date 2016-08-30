7a1d5d4a61c323bf6faab340fc2752a3800b8fc3
allanyu <googleyufei@qq.com>
2016-08-30 15:37:52 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,94 +1,57 @@
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
+var beautify_html = require('js-beautify').html;
 
-exports.name = 'upload';
-exports.desc = 'upload file or the files of destination path.';
+exports.name = 'genhtml';
+exports.desc = 'generate html for react-guide used.';
 
 exports.register = function (commander, settings) {
-    commander.option('-p, --path <path>', 'waiting upload dir')
-        .option('-f, --file <file>', 'waiting upload file')
-        .option('--verbose', 'enable verbose mode')
-        .action(function (template) {
-            var args = [].slice.call(arguments);
-            var options = args.pop();
+  commander.option('-p, --path <path>', 'waiting upload dir')
+    .option('-f, --file <file>', 'project.json file')
+    .action(function (template) {
+      var args = [].slice.call(arguments);
+      var options = args.pop();
 
-            var to = args[0],
-                distPath = options.path,
-                filePath = options.file,
-                receiver = "http://wapstatic.kf0309.3g.qq.com/receiver/receiver2.php",
-                formData;
+      var project = require(path.resolve(process.cwd(), options.file));
+      var dist = path.resolve(process.cwd(), './dist');
 
-            if (!filePath && !distPath) {
-                fis.log.error("file or path is required!");
-                return process.exit(0);
-            }
-
-            if (!to || typeof to === "object") {
-                to = '/data/wapstatic/tmp';
-            }
-            fis.log.info("options path: ", options.path, ", file: ", options.file);
-            if (distPath) {
-                distPath = path.resolve(distPath);
-                fis.log.info("Start upload path->'" + distPath + "' files");
-                explorer(distPath, function (file) {
-                    var relativePath = path.relative(distPath, file),
-                        basename = path.basename(file); //TODO 过滤文件
-                    formData = {
-                        to: to + "/" + relativePath,
-                        file: fs.createReadStream(file)
-                    };
-                    request.post({ url: receiver, formData: formData }, function optionalCallback(err, httpResponse, body) {
-                        if (err) {
-                            return fis.log.error('upload failed:', relativePath + "\n Error:" + err);
-                        }
-                        fis.log.info("Upload " + relativePath + " successful!");
-                    });
-                });
-            } else {
-                formData = {
-                    to: to,
-                    file: fs.createReadStream(path.resolve(filePath))
-                };
-                var ext = path.extname(filePath),
-                    basename = path.basename(filePath);
-                formData.to = formData.to + "/" + basename;
-                fis.log.info("start upload file ", filePath, ', to dest: ', formData.to);
-                request.post({ url: receiver, formData: formData }, function optionalCallback(err, httpResponse, body) {
-                    if (err) {
-                        return fis.log.error('upload failed:', filePath + "\n Error:" + err);
-                    }
-                    fis.log.info("Upload successful!");
-                });
-            }
+      project.entrys.map((entry) => {
+        var url = 'http://127.0.0.1:3000/' + entry.filename.substring(0, entry.filename.lastIndexOf('.'));
+        fis.log.info("start request ", url);
+        request(url, function (err, response, body) {
+          if (err) {
+            return fis.log.error('request failed, error: ', err);
+          }
+          // fis.log.info("download html success! now beautify html ...");
+          body = beautify_html(body, {
+            "indent_size": 2,
+            "indent_char": " ",
+            "eol": "\n",
+            "indent_level": 0,
+            "indent_with_tabs": false,
+            "preserve_newlines": true,
+            "max_preserve_newlines": 10,
+            "jslint_happy": false,
+            "space_after_anon_function": false,
+            "brace_style": "collapse",
+            "keep_array_indentation": false,
+            "keep_function_indentation": false,
+            "space_before_conditional": true,
+            "break_chained_methods": false,
+            "eval_code": false,
+            "unescape_strings": false,
+            "wrap_line_length": 0,
+            "wrap_attributes": "auto",
+            "wrap_attributes_indent_size": 4,
+            "end_with_newline": false
+          });
+          var htmlPath = path.resolve(dist, entry.filename);
+          fs.writeFileSync(htmlPath, body, {
+            encoding: 'utf-8'
+          });
+          fis.log.info("success generate html to ", htmlPath);
         });
-};
-
-/**
- * 读取文件路径内的所有文件
- * @param  {[string]} dpath 路径
- * @return 
- */
-function explorer(dpath, callback) {
-    fs.readdir(dpath, function (err, files) {
-        if (err) {
-            console.log("error: \n" + err);
-            return;
-        }
-
-        files.forEach(function (file) {
-            fs.stat(dpath + "\\" + file + "", function (err, stat) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-
-                if (stat.isDirectory()) {
-                    explorer(dpath + "\\" + file, callback);
-                } else {
-                    callback && callback(dpath + "\\" + file);
-                }
-            });
-        });
-    });
+      });
+    })
 }
